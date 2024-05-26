ff88c2caaefb9fa999611cf7264b6a466eb99fb1
shubham-ram <shubhamram2k@gmail.com>
2024-05-26 14:07:26 +0530

    updated layout and global.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,12 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body
+				className={inter.className}
+				style={{ width: "100vw", height: "100vh" }}
+			>
+				{children}
+			</body>
 		</html>
 	);
 }
