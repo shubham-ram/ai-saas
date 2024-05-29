b4d9c8957e26883c8900f4d47ecd041ea3f171cd
shubham-ram <shubhamram2k@gmail.com>
2024-05-30 00:18:33 +0530

    auth route

---
+++ src/app/(Authentication)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AuthLayout(props: any) {
+	return <div>{props.children}</div>;
+}
+
+export default AuthLayout;
---
+++ src/app/(Authentication)/signup/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Authentication from "@/page-components/Authentication/component";
+
+function Signup() {
+	return <Authentication isSignup />;
+}
+
+export default Signup;
