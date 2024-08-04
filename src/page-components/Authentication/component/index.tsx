b06cb52c06584c049926c1509d0e7c9a4f22ef88
shubham-ram <shubhamram2k@gmail.com>
2024-08-04 12:52:35 +0530

    testing husky

--- src/page-components/Authentication/component/index.tsx
+++ src/page-components/Authentication/component/index.tsx
@@ -4,16 +4,15 @@ import { AuroraBackground } from "@/components/ui/AuroraBackground";
 import AuthForm from "./AuthForm";
 
 interface AuthInterface {
-	isSignUp?: boolean;
+  isSignUp?: boolean;
 }
 
 function Authentication({ isSignUp = false }: AuthInterface) {
-	console.log("checking husky");
-	return (
-		<AuroraBackground>
-			<AuthForm isSignUp={isSignUp} />
-		</AuroraBackground>
-	);
+  return (
+    <AuroraBackground>
+      <AuthForm isSignUp={isSignUp} />
+    </AuroraBackground>
+  );
 }
 
 export default Authentication;
