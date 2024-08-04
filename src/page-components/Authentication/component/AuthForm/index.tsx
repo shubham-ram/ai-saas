9de04e35c2f8853ecb086b0615aa84d650218d1e
shubham-ram <shubhamram2k@gmail.com>
2024-08-04 14:43:17 +0530

    fix: checking organizing import plugin

--- src/page-components/Authentication/component/AuthForm/index.tsx
+++ src/page-components/Authentication/component/AuthForm/index.tsx
@@ -1,15 +1,14 @@
 "use client";
 
 import React from "react";
-import getAuthControls from "../../config/authFormControls";
 import { useForm } from "react-hook-form";
 
-import Footer from "./Footer";
-
-import Header from "./Header";
-import FormItem from "./FormItem";
+import getAuthControls from "../../config/authFormControls";
 import useAuthentication from "../../hook/useAuthentication";
 
+import Footer from "./Footer";
+import FormItem from "./FormItem";
+import Header from "./Header";
 import styles from "./styles.module.css";
 
 function AuthForm({ isSignUp = false }) {
