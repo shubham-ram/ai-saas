ff99797e2c66462de586f6371015439984a54eae
shubham-ram <shubhamram2k@gmail.com>
2024-08-17 00:41:25 +0530

    feat: add webpack configuration for handling mp3 files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+	webpack: (config) => {
+		config.module.rules.push({
+			test: /\.mp3$/,
+			// use: {
+			// 	loader: "file-loader",
+			// },
+			type: "asset/resource",
+		});
+		return config;
+	},
+};
 
 export default nextConfig;
