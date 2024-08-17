a42de04f29bba1387b8759123ae686fa6d83d86a
shubham-ram <shubhamram2k@gmail.com>
2024-08-17 20:32:04 +0530

    fix: resolve ESLint issues in file

--- src/page-components/Authentication/config/authFormControls.js
+++ src/page-components/Authentication/config/authFormControls.js
@@ -1,66 +1,67 @@
 const loginControls = [
-    {
-        name: "email",
-        label: "Email Address*",
-        type: 'text',
-        placeholder: "Enter your email address",
-        rules: {
-            required: true
-        }
-    },
-    {
-        name: "password",
-        label: "Password*",
-        type: 'password',
-        subLabel: "Forgot password?",
-        placeholder: "Enter your password",
-        rules: {
-            required: true,
-            maxLength: 8
-        }
-    },
-    {
-        name: "rememberMe",
-        label: "Remember Me",
-        showLabel: false,
-        type: 'checkbox',
-        span: 6
-    }
-]
+	{
+		name: "email",
+		label: "Email Address*",
+		type: "text",
+		placeholder: "Enter your email address",
+		rules: {
+			required: "Email is Mandatory",
+		},
+	},
+	{
+		name: "password",
+		label: "Password*",
+		type: "password",
+		subLabel: "Forgot password?",
+		placeholder: "Enter your password",
+		rules: {
+			required: "Password is Mandatory",
+			maxLength: 8,
+		},
+	},
+	{
+		name: "rememberMe",
+		label: "Remember Me",
+		showLabel: false,
+		type: "checkbox",
+		span: 6,
+	},
+];
 const signupControls = [
-    {
-        name: "name",
-        label: "Name*",
-        type: 'text',
-        placeholder: "Enter your name",
-        rules: {
-            required: true
-        }
-    },
-    {
-        name: "email",
-        label: "Email Address*",
-        type: 'text',
-        placeholder: "Enter your email address",
-        rules: {
-            required: true
-        }
-    },
-    {
-        name: "password",
-        label: "Password*",
-        type: 'password',
-        bottomSubLabel: "(Note: Your password must contain 8 or more characters)",
-        placeholder: "Enter your password",
-        rules: {
-            required: true,
-            maxLength: 8
-        }
-    },
-]
+	{
+		name: "name",
+		label: "Name*",
+		type: "text",
+		placeholder: "Enter your name",
+		rules: {
+			required: "Name is mandatory",
+		},
+	},
+	{
+		name: "email",
+		label: "Email Address*",
+		type: "text",
+		placeholder: "Enter your email address",
+		rules: {
+			required: "Email is mandatory",
+		},
+	},
+	{
+		name: "password",
+		label: "Password*",
+		type: "password",
+		bottomSubLabel:
+			"(Note: Your password must contain 8 or more characters)",
+		placeholder: "Enter your password",
+		rules: {
+			required: "Please Enter Correct Password",
+			maxLength: 8,
+		},
+	},
+];
 
 function getAuthControls({ isSignUp = false }) {
-    return isSignUp ? signupControls : loginControls
+	return isSignUp ? signupControls : loginControls;
 }
 
 export default getAuthControls;
